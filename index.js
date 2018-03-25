ba1eee488a3101da872f0bec074a538ea74a5b8b
charlie <5010456@qq.com>
2018-03-25 21:50:44 +0800

    Node.js project files

---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const fs = require('fs')
+const Promise = require('promise')
+const request = require('request')
+
+const requestUrl = 'http://huaban.com/favorite/beauty/?jf5ga5b7&max=1567407240&limit=100&wfl=1'
+
+function getHtml(url) {
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let html = ''
+            res.setEncoding('utf-8')
+            res.on('data', chunk => {
+                html += chunk
+            })
+            res.on('end', () => {
+                resolve(html)
+            })
+        }).on('error', e => {
+            reject(e)
+        })
+    })
+}
+
+function writeFile(uri, filename) {    
+    request.head(uri, (err, res, body) => {
+        if (err) {
+                
+        }
+    })
+    console.log('\n-----STRAT DOWNLOAD-----\n')
+    console.log('-FILENAME: ' + filename + '\n')
+    request(uri).pipe(fs.createWriteStream('./image/' + filename))
+    console.log('-----ENDED DOWNLOAD-----\n')   
+}
+
+async function getData () {
+    let html = await getHtml(requestUrl)
+    let arr = html.match(/"key":"(\w+-\w+)",\s"type":"image\/jpeg"/g).concat()
+    arr.forEach(ele => {
+        let uri = 'http://img.hb.aicdn.com/' + ele.match(/(\w+-\w+)/)[1]
+        let filename = ele.match(/(\w+-\w+)/)[1] + '.jpeg'
+        writeFile(uri, filename)
+    })
+}
+
+getData()
