2b453bf5495c20c48d211bae856a7d0121afb86f
Charlie <5010456@qq.com>
2019-01-14 17:17:06 +0800

    skip download if ever downloaded

--- modules/downloadFile.js
+++ modules/downloadFile.js
@@ -1,13 +1,21 @@
 const download = require('download')
 const Promise = require('promise')
+const { pathIsExist } = require('./pathIsExist')
 
 const downloadFile =  (urlQueue, crawlPicCount, distPath, filenameQueue) => {
   let downloadUrlQueue = urlQueue
-  if (crawlPicCount && (crawlPicCount <= urlQueue.length)) {
+  if (crawlPicCount && (crawlPicCount < urlQueue.length)) {
     downloadUrlQueue = urlQueue.slice(0, crawlPicCount)
     console.log(`Preparing download ${crawlPicCount} images.`)
   }
-  Promise.all(downloadUrlQueue.map((f, i) => download(f, distPath, { filename: filenameQueue[i] }))).then(r => {
+  let downloadQueue = filenameQueue.map((filename, i) => {
+    if (pathIsExist(`${distPath}/${filename}`)) {
+      console.log(`Skipping file "${filename}"`)
+      return
+    }
+    return downloadUrlQueue[i]
+  })
+  Promise.all(downloadQueue.map((f, i) => download(f, distPath, { filename: filenameQueue[i] }))).then(r => {
       console.log('Download Complete.')
   })
 }
