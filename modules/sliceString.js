e6fd4b3ba35d9253a9db9ed1f73890b2e3969a72
Charlie <5010456@qq.com>
2019-01-15 14:37:50 +0800

    create folder 'config' to keep config.js

--- modules/sliceString.js
+++
@@ -1,7 +0,0 @@
-const sliceString = (string, charHead, charTail) => 
-    string.slice(
-        string.indexOf(charHead) + charHead.length || 0
-        , string.indexOf(charTail) || string.length - 1
-    )
-
-module.exports = { sliceString }
